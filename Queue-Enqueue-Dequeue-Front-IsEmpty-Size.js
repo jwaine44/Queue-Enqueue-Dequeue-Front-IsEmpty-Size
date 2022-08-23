7b9c8ad5d08ce4107194cea4101c3519e51373cd
jwaine44 <jwaine44@gmail.com>
2022-08-23 11:47:53 -0400

    Initialize Repo

---
+++ Queue-Enqueue-Dequeue-Front-IsEmpty-Size.js
@@ -0,0 +1,84 @@
+/**
+ * Class to represent a queue using an array to store the queued items.
+ * Follows a FIFO (First In First Out) order where new items are added to the
+ * back and items are removed from the front.
+ */
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+
+    /**
+     * Adds a new given item to the back of this queue.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @param {any} item The new item to add to the back.
+     * @returns {number} The new size of this queue.
+     */
+    enqueue(item){
+        this.items.push(item);
+        return this.items.length;
+    }
+
+    /**
+     * Removes and returns the first item of this queue.
+     * - Time: O(n) linear, due to having to shift all the remaining items to
+     *    the left after removing first elem.
+     * - Space: O(1) constant.
+     * @returns {any} The first item or undefined if empty.
+     */
+    dequeue(){
+        if(this.isEmpty()){
+            return undefined;
+        }
+        return this.items.shift();
+    }
+
+    /**
+     * Retrieves the first item without removing it.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {any} The first item or undefined if empty.
+     */
+    front(){
+        if(this.isEmpty()){
+            return undefined;
+        }
+        return this.items[0];
+    }
+
+    /**
+     * Returns whether or not this queue is empty.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {boolean}
+     */
+    isEmpty(){
+        if(this.items.length == 0){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Retrieves the size of this queue.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {number} The length.
+     */
+    size(){
+        return this.items.length;
+    }
+}
+
+let newQ = new Queue();
+newQ.enqueue(33);
+newQ.enqueue(5);
+newQ.enqueue(154);
+newQ.enqueue(98);
+
+
+console.log(newQ);
+console.log(newQ.dequeue());
+console.log(newQ.size());
+console.log(newQ.front());
